a4a1ca15aecd6db8b8af44f2e450b43b1cf107f7
kyu08 <49891479+kyu08@users.noreply.github.com>
2023-01-12 01:32:51 +0900

    delete log

--- cmd/mygit/cat_file.go
+++ cmd/mygit/cat_file.go
@@ -13,8 +13,6 @@ import (
 func catFile(opt, optValue *string) {
 	switch *opt {
 	case "-p":
-		fmt.Print("[START] catFile\n")
-		defer fmt.Print("[END] catFile\n")
 		const hashLen = 40
 
 		if optValue == nil {
@@ -27,7 +25,6 @@ func catFile(opt, optValue *string) {
 			panic("invalid hash format.")
 		}
 		filePath := fmt.Sprintf(".git/objects/%s/%s", blobHash[:2], blobHash[2:])
-		fmt.Print("generate filePath\n")
 
 		// ファイル内容を取得
 		b, err := ioutil.ReadFile(filePath)
@@ -35,7 +32,6 @@ func catFile(opt, optValue *string) {
 			fmt.Printf("ReadFile failed: %s\n", err)
 			os.Exit(1)
 		}
-		fmt.Print("ReadFile\n")
 
 		// ファイル内容を解凍
 		result, err := unzip(b)
@@ -43,7 +39,6 @@ func catFile(opt, optValue *string) {
 			fmt.Printf("unzipLines failed: %s\n", err)
 			os.Exit(1)
 		}
-		fmt.Print("Unzip\n")
 
 		fmt.Printf(result)
 	}
