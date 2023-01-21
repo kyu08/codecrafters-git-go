d85b7bd63b4c3995b6ec4260fefc9eb232f9e0b9
kyu08 <49891479+kyu08@users.noreply.github.com>
2023-01-21 13:33:33 +0900

    file content

--- cmd/mygit/ls_tree.go
+++ cmd/mygit/ls_tree.go
@@ -2,15 +2,26 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 )
 
 func lsTree(opt, optValue *string) {
 	switch *opt {
 	case "--name-only":
-		fmt.Fprintf(os.Stderr, "sha: %s\n", *optValue)
-		fmt.Printf("optValue: %v\n", optValue)
 		// tree-shaをファイルパスに変換
+		hash := *optValue
+		filePath := fmt.Sprintf(".git/objects/%s/%s", hash[:2], hash[2:])
+
+		// ファイル内容を取得
+		b, err := ioutil.ReadFile(filePath)
+		if err != nil {
+			fmt.Printf("ReadFile failed: %s\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("string(b): %v\n", string(b))
+
 		// treeオブジェクトのファイル内容を取得
 		// blobならファイル名をtreeなら再帰で処理
 		// アルファベット順にソート
