e1b3f268442a7c641cfd3113965931ffda57ca0d
kyu08 <49891479+kyu08@users.noreply.github.com>
2023-01-29 23:29:53 +0900

    delete unused lines

--- cmd/mygit/handler/handler.go
+++ cmd/mygit/handler/handler.go
@@ -4,7 +4,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// fmt.Printで標準出力固定で出力するのではなく、fmt.Fprintfとかを使って出力先を外から渡すようにすればよりテスタブルになりそう
 func Command() *cobra.Command {
 	var rootCmd = &cobra.Command{Use: "mygit usage"}
 	rootCmd.AddCommand(Init())
@@ -13,12 +12,4 @@ func Command() *cobra.Command {
 	rootCmd.AddCommand(LSTree())
 
 	return rootCmd
-}
-
-func getElem(args []string, index int64) *string {
-	if int64(len(args)) >= (index + 1) {
-		return &args[index]
-	}
-
-	return nil
 }
